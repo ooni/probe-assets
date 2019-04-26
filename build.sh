afca612fcb3bfc6e52ea481d5ba91eb7d72ee67e
Simone Basso <bassosimone@gmail.com>
2019-04-26 15:58:25 +0200

    Build in subdirectory and save SHA256SUM
    Closes #2
    
    Closes #3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -ex
+set -e
 
 # autogen_get_geoip is copied from MK v0.10.0. It downloads the
 # maxmind DB databases from MaxMind's site.
@@ -10,22 +10,31 @@ autogen_get_geoip() {
         curl -fsSLO $base/GeoLite2-Country.tar.gz
         tar -xf GeoLite2-Country.tar.gz
         mv GeoLite2-Country_20??????/GeoLite2-Country.mmdb country.mmdb
-        rm -rf GeoLite2-Country_20??????
+        rm -rf GeoLite2-Country_20?????? GeoLite2-Country.tar.gz
     fi
     if [ ! -f "asn.mmdb" ]; then
         curl -fsSLO $base/GeoLite2-ASN.tar.gz
         tar -xf GeoLite2-ASN.tar.gz
         mv GeoLite2-ASN_20??????/GeoLite2-ASN.mmdb asn.mmdb
-        rm -rf GeoLite2-ASN_20??????
+        rm -rf GeoLite2-ASN_20?????? GeoLite2-ASN.tar.gz
     fi
 }
 
+rm -rf assets
+mkdir assets
 version=`date +%Y%m%d`
 autogen_get_geoip
 curl -fsSLo ca-bundle.pem https://curl.haxx.se/ca/cacert.pem
-tar -cvzf "generic-assets-${version}.tar.gz"                                   \
+tar -cvzf "assets/generic-assets-${version}.tar.gz"                            \
   "README.md" "asn.mmdb" "ca-bundle.pem" "country.mmdb"
+rm ca-bundle.pem
 gzip -9 asn.mmdb
+mv asn.mmdb.gz assets/
 gzip -9 country.mmdb
-echo "# git commit -am \"Release $version\""
-echo "# git tag -s $version"
+mv country.mmdb.gz assets/
+shasum -a 256 assets/* > SHA256SUMS
+git add SHA256SUMS
+echo "# To continue with the release run"
+echo "- git commit -am \"Release $version\""
+echo "- git tag -sm \"measurement-kit/generic-assets $version\" $version"
+echo "- git push origin master $version"
