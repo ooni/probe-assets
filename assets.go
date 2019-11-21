baf0644616b8ab9cbe8401a12bd0956636e78194
Simone Basso <bassosimone@gmail.com>
2019-11-21 16:26:46 +0100

    Release 20191121162556

--- assets.go
+++ assets.go
@@ -2,7 +2,7 @@ package resources
 
 const (
 	// Version contains the assets version.
-	Version = 20191114173737
+	Version = 20191121162556
 
 	// ASNDatabaseName is the ASN-DB file name
 	ASNDatabaseName = "asn.mmdb"
@@ -33,18 +33,18 @@ type ResourceInfo struct {
 // All contains info on all known assets.
 var All = map[string]ResourceInfo{
 	"asn.mmdb": ResourceInfo{
-		URLPath:  "/releases/download/20191114173737/asn.mmdb.gz",
-		GzSHA256: "ee7b8b939d74dd75fc58eb1a39eae833c2ba11200c46640977ff8e825e2f0c53",
-		SHA256:   "bf9943b5bc6d36573dad1bca1e5d9a3bc523620700b33efbe0261f8fe4aaa967",
+		URLPath:  "/releases/download/20191121162556/asn.mmdb.gz",
+		GzSHA256: "ae53fa9d5a7bbba4c26d90df7604054e72e0c80fac4a3fb4d03f603d6e370bca",
+		SHA256:   "d5c08af2011a4e6559e04317773198c72e9ed4b749efe73dfc66097ab01196b5",
 	},
 	"ca-bundle.pem": ResourceInfo{
-		URLPath:  "/releases/download/20191114173737/ca-bundle.pem.gz",
-		GzSHA256: "07e47ee9bffe655fdaee5c1a369b9580a53401c1dfde28a02664a40973eb369c",
+		URLPath:  "/releases/download/20191121162556/ca-bundle.pem.gz",
+		GzSHA256: "2ef0d342c1a451ee0a4e3b14a65faf7b3ecc649a0cb4e49d23be560417d1af4f",
 		SHA256:   "5cd8052fcf548ba7e08899d8458a32942bf70450c9af67a0850b4c711804a2e4",
 	},
 	"country.mmdb": ResourceInfo{
-		URLPath:  "/releases/download/20191114173737/country.mmdb.gz",
-		GzSHA256: "33af311275634fb686308e3b7e49b8c0ecdc4e2b152a29abeed948a3a0b82152",
-		SHA256:   "1c668e490cdd84b140179d852c19116a35ec8612dd3beabeff7ce1dce536fd30",
+		URLPath:  "/releases/download/20191121162556/country.mmdb.gz",
+		GzSHA256: "5172eaef2fd2246e224dea6bff02af29b1d7d5c99b9c511eb765582273f027a8",
+		SHA256:   "057ba471fde4a0007428ef1d0efb93e04a644eb3f6ace8a775fc842168e95af9",
 	},
 }
