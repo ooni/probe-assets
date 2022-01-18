21700d6804fcb1f549bc02b06bba4bbd53da7bd3
Simone Basso <bassosimone@gmail.com>
2022-01-18 13:55:54 +0100

    chore: use https://db-ip.com/ also for the ASN database
    While there, attempt to make the release process a bit leaner
    by executing most commands and living little to do for the human
    being who's performing the release.
    
    See https://github.com/ooni/probe/issues/1959.

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-# Variables you should set when updating. You SHOULD probably go to
-# https://github.com/ooni/asn-db-generator and follow the release
-# procedure described in README.md to generate a new asn.mmdb file.
-#
-# Regarding the country database, you SHOULD check if a new version
-# is available, update the URL and the sha1sum (they don't provide
-# anything better than SHA1, so I guess we have to live with it).
-country_db_url=https://download.db-ip.com/free/dbip-country-lite-2021-12.mmdb.gz
-country_db_sha1sum=d068bcdad6c7055cb2e95c6914a67cf3a87b6254
-asn_db_url=https://github.com/ooni/asn-db-generator/releases/download/20211206152521/asn.mmdb.gz
-asn_db_sha256sum=1175024a83d5601e8aeaf34a9d3965137715df7ac87f604669b0090fa77db5d2
+# Variables you should set when updating. You SHOULD check on
+# https://db-ip.com/ if a new version is available.
+country_db_url=https://download.db-ip.com/free/dbip-country-lite-2022-01.mmdb.gz
+country_db_sha1sum=f86e40d74cdd14ef64a966bcd7b9c5fd835f3187
+asn_db_url=https://download.db-ip.com/free/dbip-asn-lite-2022-01.mmdb.gz
+asn_db_sha1sum=0f6e7317e124dcc532011dea3abfc32b73983a91
+
+# Remove leftovers.
+set -x
+rm -f ./assets/*.mmdb ./assets/*.mmdb.gz
+set +x
 
 # See https://remarkablemark.org/blog/2017/10/12/check-git-dirty/
 [[ -z `git status -s` ]] || {
@@ -25,11 +25,6 @@ asn_db_sha256sum=1175024a83d5601e8aeaf34a9d3965137715df7ac87f604669b0090fa77db5d
   echo "FATAL: not on the master branch" 1>&2
   exit 1
 }
-
-# Remove leftovers.
-set -x
-rm -f ./assets/*.mmdb ./assets/*.mmdb.gz
-set +x
 
 # Fetch the country DB file, decompress and verify it.
 country_db_file=./assets/country.mmdb
@@ -45,32 +40,41 @@ if [ "$sha1sum" != "$country_db_sha1sum" ]; then
 fi
 
 # Fetch the asn DB file, verify and decompress it.
-asn_db_gzfile=./assets/asn.mmdb.gz
+asn_db_file=./assets/asn.mmdb
+asn_db_gzfile=${asn_db_file}.gz
 set -x
 curl -fsSLo $asn_db_gzfile $asn_db_url
+gunzip -k $asn_db_gzfile
 set +x
-sha256sum=`shasum -a256 $asn_db_gzfile | awk '{print $1}'`
-if [ "$sha256sum" != "$asn_db_sha256sum" ]; then
-  echo "FATAL: asn database does not match the expected sha256sum" 1>&2
+sha1sum=`shasum -a1 $asn_db_file | awk '{print $1}'`
+if [ "$sha1sum" != "$asn_db_sha1sum" ]; then
+  echo "FATAL: asn database does not match the expected sha1sum" 1>&2
   exit 1
 fi
-gunzip -k $asn_db_gzfile
 
 # Make sure what we have downloaded does not emit smoke.
+set -x
 go test -v ./...
+set +x
+
+if [ "$1" = "-n" ]; then
+  exit 0  # dry-run
+fi
 
 # Instructions to make a release.
 version=`cat VERSION`
 version=$(($version + 1))
 echo $version > VERSION
+set -x
 git commit -m "Bump version to 0.$version.0" VERSION
+git checkout -b vendor-0.$version.0
+git add assets/*.mmdb
+git commit -am "Release 0.$version.0"
+git tag -m "Tag v0.$version.0" -s v0.$version.0
+set +x
 echo "Now you should run the following commands:"
 echo ""
-echo "- git checkout -b vendor-0.$version.0"
-echo "- git add assets/*.mmdb"
-echo "- git commit -am \"Release 0.$version.0\""
-echo "- git tag -s v0.$version.0"
-echo "- git push origin vendor-0.$version.0 v0.$version.0"
+echo "- git push origin master vendor-0.$version.0 v0.$version.0"
 echo ""
 echo "For updating, you need to go in github.com/ooni/probe-cli and run:"
 echo ""
