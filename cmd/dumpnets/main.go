758f6cb01ba2e239ff86899bd73bbb0ed5537330
Simone Basso <bassosimone@gmail.com>
2022-09-09 08:24:41 +0200

    feat(cmd/dumpnets): dump all networks inside a DB
    This newly added command dumps all the networks inside a DB
    using a CSV format. After writing the code, I realized it is
    also possible to download files in CSV format from db-ip,
    yet I think it makes sense for us to operate on the actual
    binary databases we will ship, so...
    
    Part of https://github.com/ooni/probe/issues/2271

---
+++ cmd/dumpnets/main.go
@@ -0,0 +1,37 @@
+// Command dumpnets dumps the networks inside a MaxMind database.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/oschwald/geoip2-golang"
+	"github.com/oschwald/maxminddb-golang"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: ./dumpnets {database}\n")
+		os.Exit(1)
+	}
+	filename := os.Args[1]
+	mmdb, err := maxminddb.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "warning: maxminddb.Open %s: %s\n", filename, err.Error())
+		os.Exit(1)
+	}
+	nets := mmdb.Networks(maxminddb.SkipAliasedNetworks)
+	for nets.Next() {
+		var asn geoip2.ASN
+		net, err := nets.Network(&asn)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fatal: nets.Network: %s\n", err.Error())
+			os.Exit(1)
+		}
+		fmt.Printf("%s, %d\n", net.String(), asn.AutonomousSystemNumber)
+	}
+	if err := nets.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "fatal: nets.Err: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
