6ae4617f2d30fad1707e598d057702ac773a390e
Simone Basso <bassosimone@gmail.com>
2024-03-22 14:54:06 +0100

    chore: use 2024-03 database

--- prepare.bash
+++ prepare.bash
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 # Variables you should set when updating. You SHOULD check on
 # https://db-ip.com/ if a new version is available.
-db_url=https://archive.org/download/ip2country-as/20231201-ip2country_as.mmdb.gz
-db_sha256=23a27f7a3bbb3ba9d74d4e09b71fc063ba40f527dc2601886e3b5ece9180ac51
+db_url=https://archive.org/download/ip2country-as/20240301-ip2country_as.mmdb.gz
+db_sha256=f510616ad888fd9ae096794294f1ba6f8a06afaaef8f511f874ae6a72f3fca69
 
 # Remove leftovers.
 rm -f ./assets/*.mmdb ./assets/*.mmdb.gz
