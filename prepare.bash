adc0476e7a196506506361fb943de615c7f2ee4f
Simone Basso <bassosimone@gmail.com>
2024-05-13 18:37:58 +0200

    chore: update to 2024-05 database
    See https://github.com/ooni/probe/issues/2722

--- prepare.bash
+++ prepare.bash
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 # Variables you should set when updating. You SHOULD check on
 # https://db-ip.com/ if a new version is available.
-db_url=https://archive.org/download/ip2country-as/20240301-ip2country_as.mmdb.gz
-db_sha256=f510616ad888fd9ae096794294f1ba6f8a06afaaef8f511f874ae6a72f3fca69
+db_url=https://archive.org/download/ip2country-as/20240501-ip2country_as.mmdb.gz
+db_sha256=bc8b06049e959ba010075ab11ac309002e45623869d02d43f86e270fc88981c0
 
 # Remove leftovers.
 rm -f ./assets/*.mmdb ./assets/*.mmdb.gz
